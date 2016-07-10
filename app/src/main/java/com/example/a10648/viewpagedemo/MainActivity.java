1835c681007b6452adbf7e7c467e99f55c301cae
lizheng <lixiaozheng.good@163.com>
2016-07-10 09:08:24 -0700

    simple usage of viewpage
    add OneActivity with layoutut_one
    add MyPageAdapter
    enable onclick button1

--- app/src/main/java/com/example/a10648/viewpagedemo/MainActivity.java
+++ app/src/main/java/com/example/a10648/viewpagedemo/MainActivity.java
@@ -1,13 +1,46 @@
 package com.example.a10648.viewpagedemo;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+
+    @InjectView(R.id.btn_one)
+    Button btnOne;
+    @InjectView(R.id.btn_two)
+    Button btnTwo;
+    @InjectView(R.id.btn_three)
+    Button btnThree;
+    @InjectView(R.id.btn_four)
+    Button btnFour;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.inject(this);
+    }
+
+    @OnClick({R.id.btn_one, R.id.btn_two, R.id.btn_three, R.id.btn_four})
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.btn_one:
+                Intent intent = new Intent(MainActivity.this, OneActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.btn_two:
+                break;
+            case R.id.btn_three:
+                break;
+            case R.id.btn_four:
+                break;
+        }
     }
 }
---
+++ app/src/main/java/com/example/a10648/viewpagedemo/OneActivity.java
@@ -0,0 +1,45 @@
+package com.example.a10648.viewpagedemo;
+
+import android.os.Bundle;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.LinearLayout;
+
+import java.util.ArrayList;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+
+public class OneActivity extends AppCompatActivity {
+
+
+    @InjectView(R.id.vpager_one)
+    ViewPager vpagerOne;
+
+    View view1, view2, view3;
+    ArrayList<View> views = new ArrayList<>();//是否等价于 ArrayList<View> views = new ArrayList<>();若是ArrayList<View> views ;会报NPE异常
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_one);
+        ButterKnife.inject(this);
+
+        view1 = LayoutInflater.from(this).inflate(R.layout.view_one, null, false);
+        view2 = LayoutInflater.from(this).inflate(R.layout.view_two, null, false);
+        view3 = LayoutInflater.from(this).inflate(R.layout.view_three, null, false);
+
+        views.add(view1);
+        views.add(view2);
+        views.add(view3);
+
+        MyPagerAdapter myPagerAdapter = new MyPagerAdapter(views);
+        vpagerOne.setAdapter(myPagerAdapter);
+
+
+    }
+}
